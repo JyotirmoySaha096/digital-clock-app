8101d708e0d2a2586bcc4f31bedb6e538ec85702
Jyotirmoy <jyotirmoysaha096@gmail.com>
2022-12-16 00:25:37 +0530

    Created a Simple clock app which shows time and date.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
+const weekdayGenerator = weekday => {
+  switch(weekday){
+    case 0:
+      return "Sunday";
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednessday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    default:
+      return "Invalid day";
+  }
+}
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 function App() {
+  let date = new Date();
+  let weekday = date.getDay();
+  let month = months[date.getMonth()];
+  let dateDay = date.getDate()+1;
+  let year = date.getFullYear();
+
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <h3>A SIMPLE DIGITAL CLOCK</h3>
+      <h1>{hours+":"+minutes+":"+seconds}</h1>
+      <h2>{weekdayGenerator(weekday) + ", " + month + " " + dateDay + "th " + year}</h2>
+      <button className='btn'>Switch to military time format.</button>
     </div>
+    </>
   );
 }
 
